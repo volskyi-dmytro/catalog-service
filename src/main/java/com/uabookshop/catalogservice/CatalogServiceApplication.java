0025b6af83ca871096932b900746e0a49bc33417
@stpunk47 <stpunk47@gmail.com>
2024-01-06 23:41:14 +0200

    Initial project setup

---
+++ src/main/java/com/uabookshop/catalogservice/CatalogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uabookshop.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogServiceApplication.class, args);
+	}
+
+}
