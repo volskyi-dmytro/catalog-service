8908a7981800625b6d43af8af1b364aae3c3a4e7
@stpunk47 <stpunk47@gmail.com>
2024-01-10 00:22:19 +0200

    Added Unit tests to verify book validation constraints

---
+++ src/test/java/com/uabookshop/catalogservice/BookValidationTests.java
@@ -0,0 +1,43 @@
+package com.uabookshop.catalogservice;
+
+import com.uabookshop.catalogservice.domain.Book;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
+class BookValidationTests {
+    private static Validator validator;
+
+    @BeforeAll
+    static void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    void whenAllFieldsCorrectThenValidationSucceeds() {
+        var book = new Book(
+                "1234567890", "Title", "Author", 5.55);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).isEmpty();
+    }
+
+    @Test
+    void whenIsbnDefinedButIncorrectThenValidationFails() {
+        var book = new Book(
+                "asdf111", "Title", "Author", 3.33);
+        Set<ConstraintViolation<Book>> violations = validator.validate(book);
+        assertThat(violations).hasSize(1);
+        assertThat(
+                violations
+                        .iterator()
+                        .next()
+                        .getMessage()).isEqualTo("The ISBN format must be valid.");
+    }
+}
