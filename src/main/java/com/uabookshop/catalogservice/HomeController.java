0180419ae70d880e4e6b9432a1b358f829120af4
@stpunk47 <stpunk47@gmail.com>
2024-01-19 00:43:05 +0200

    Added custom properties testing

--- src/main/java/com/uabookshop/catalogservice/HomeController.java
+++ src/main/java/com/uabookshop/catalogservice/HomeController.java
@@ -1,13 +1,20 @@
 package com.uabookshop.catalogservice;
 
+import com.uabookshop.catalogservice.config.UAbookshopProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    private final UAbookshopProperties uAbookshopProperties;
+
+    public HomeController(UAbookshopProperties uAbookshopProperties) {
+        this.uAbookshopProperties = uAbookshopProperties;
+    }
+
+
     @GetMapping("/hello")
     public String getGreeting() {
-        return "Welcome to the book catalog!";
-    }
+        return uAbookshopProperties.getGreeting();    }
 }
