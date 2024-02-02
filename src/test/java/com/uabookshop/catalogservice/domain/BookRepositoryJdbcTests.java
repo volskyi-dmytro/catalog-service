68f1bec48b03f69ac86640a3dab708477d447f99
volskyi-dmytro <volskyi.dmytro@gmail.com>
2024-02-02 15:51:55 +0000

    Added data slice test

---
+++ src/test/java/com/uabookshop/catalogservice/domain/BookRepositoryJdbcTests.java
@@ -0,0 +1,42 @@
+package com.uabookshop.catalogservice.domain;
+
+import com.uabookshop.catalogservice.config.DataConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jdbc.core.JdbcAggregateTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJdbcTest
+@Import(DataConfig.class)
+@AutoConfigureTestDatabase(
+        replace = AutoConfigureTestDatabase.Replace.NONE
+)
+@ActiveProfiles("integration")
+class BookRepositoryJdbcTests {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    @Autowired
+    private JdbcAggregateTemplate jdbcAggregateTemplate;
+
+    @Test
+    void findBookByIsbnWhenExisting() {
+
+        var bookIsbn = "1234561237";
+        var book = Book.of(bookIsbn, "Title", "Author", 11.32);
+        jdbcAggregateTemplate.insert(book);
+
+        Optional<Book> actualBook = bookRepository.findByIsbn(bookIsbn);
+
+        assertThat(actualBook).isPresent();
+        assertThat(actualBook.get().isbn()).isEqualTo(book.isbn());
+    }
+}
